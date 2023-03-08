721c533ff7223b02436d60c6f9faa0a4d870b3df
misakawang <misakawang@tencent.com>
2023-03-08 10:31:12 +0800

    test1

--- Ethereal/Private/Characters/Enemy/Boss/GaiaTitan.cpp
+++ Ethereal/Private/Characters/Enemy/Boss/GaiaTitan.cpp
@@ -18,6 +18,17 @@
 
 #define LOCTEXT_NAMESPACE "EtherealText"
 
+class Box
+{
+   public:
+      double length;   // 长度
+      double breadth;  // 宽度
+      double height;   // 高度
+      // 成员函数声明
+      double get(void);
+      void set( double len, double bre, double hei );
+};
+
 AGaiaTitan::AGaiaTitan(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
